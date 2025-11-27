4b783d433bb8feb7d6035a1e6575299556aa9caa
Manshi <manshi@dure.dev>
2025-11-27 13:33:11 +0530

    add vercel configuration for deployement

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import express from 'express'; // Default import
+
+const server = express();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
 
-  await app.listen(process.env.PORT || 3000);
-  console.log(`🚀 Server running on port ${process.env.PORT}`);
+  await app.init();
 }
 
 bootstrap();
+
+export default server;
